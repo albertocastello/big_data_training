ff3c3018598a51826d9132bc5f6413a2e68a6a70
Alberto <albertocastello@gmail.com>
2025-06-13 01:25:20 +0200

    created main_script.R file for loading packages

---
+++ R/main_script.R
@@ -0,0 +1,10 @@
+# install packages
+#install.packages(c("data.table", "purrr", "furrr", "sf", "parallel", "arrow"), dependencies = TRUE)
+
+# load packages
+library(data.table)
+library(purrr)
+library(furrr)
+library(sf)
+library(parallel)
+library(arrow)
